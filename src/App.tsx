3ebe44ac24abc1df07819ed727f4f6e76c14d60a
Paul-GB <vctranfr@gmail.com>
2023-11-06 13:09:55 -0500

    Section 13 - Find books by title with pagination completed

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Navbar } from './layouts/NavBarAndFooter/Navbar';
+import { Footer } from './layouts/NavBarAndFooter/Footer';
+import { HomePage } from './layouts/HomePage/HomePage';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+
+export const App = () => {
+  return (
+    <div>
+      <Navbar />
+      { /* <HomePage /> */ }
+      <SearchBooksPage />
+      <Footer />
+    </div>
+  );
+}
